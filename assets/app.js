9f65f530b57be696b6a11ec597ce5b5ec273a53f
sorrow112 <jguirimahmed111@gmail.com>
2022-02-15 14:51:07 +0000

    react activated + everything is set up for work

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,16 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import ReactDOM from 'react-dom';
 
-// start the Stimulus application
-import './bootstrap';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div> <h1> react works!</h1> </div>
+  )
+}
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
